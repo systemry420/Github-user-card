9866546622f7ef99c42ce551af8f94450e37e687
Hasan <systemry420@gmail.com>
2020-09-10 20:17:29 +0300

    general styling

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // get github api
 const APIURL = 'https://api.github.com/users/';
 const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 getUsers('systemry420');
 
 
@@ -16,22 +18,34 @@ async function getUsers(user){
 
 function showCard(user){
     const card = document.createElement('div');
-    card.classList.add('card');
 
     card.innerHTML = `
-        <div>
-            <img src="${user.avatar_url}" alt="">
+        <div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="">
+            </div>
+            <div>
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul id="info">
+                    <li>${user.followers} followers</li>
+                    <li>${user.following} following</li>
+                    <li>${user.public_repos} repos</li>
+            </div>
         </div>
-        <div>
-            <h2>${user.name}</h2>
-            <p>${user.bio}</p>
-            <ul id="info">
-                <li>${user.followers}</li>
-                <li>${user.following}</li>
-                <li>${user.repos_url}</li>
-        </div>
-        
     `;
 
-    main.appendChild(card);
+    main.append(card);
 }
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const input = search.value;
+
+    if(input){
+        getUsers(input);
+        search.value = '';
+        main.innerHTML = '';
+    }
+})
